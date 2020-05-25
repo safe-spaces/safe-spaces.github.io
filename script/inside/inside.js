f0310c504f89eb2b6d9014dbec88e70ee1ada62b
azzadri007 <azzadri007@gmail.com>
2020-05-25 16:34:14 +0100

    minor adj

--- script/inside/inside.js
+++ script/inside/inside.js
@@ -10,20 +10,29 @@ $(document).ready(async function (){
 
     $('.door').on({
         'click': async function(){
+            var x = event.clientX;
+            var y = event.clientY;
+            console.log(x,' by ', y);
+    
+            $('.click').css({'position':'absolute','left':(x+'px'),'top':(y+'px'), 'display':'block'});
+    
 
             var source = './css/inside/'+ door_type[i];
             $('.door').attr('src',source);
 
-            await sleep(1000);
+            await sleep(300);
 
             var source = './css/inside/'+ door_type[i+1];
             $('.door').attr('src',source);
             
             await sleep(1000);
             var source = './css/inside/'+ door_type[i+2];
-            $('.door').attr({'src':source});
-        
+            $('.door').attr({'src':source}); 
+
+            await sleep(200);
+            $('.enter').css({'opacity':'0'}); 
+            $('.click').css({'opacity':'0'}); 
+
         }
     });
-
 });

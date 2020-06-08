b51d0751252445a377e1dbfb1b76b19e7b6b25d0
azzadri007 <azzadri007@gmail.com>
2020-06-08 12:11:48 +0100

    some addition to speed

--- script/inside/inside.js
+++ script/inside/inside.js
@@ -2,35 +2,43 @@
 var door_type = ['closed.png','down.png','open.png'];
 var i = 0;
 
+
+
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
   }
 
-$(document).ready(async function (){
 
-    $('.door').on({
-        'click': async function(){
 
-                var x = event.clientX;
-                var y = event.clientY;
-                console.log(x,' by ', y);
+
+    $(window).bind("load", async function() {
+        console.log('finally')
+        $('.door').on({
+            'click': async function(){
+    
+                console.log('click')
+                    var x = event.clientX;
+                    var y = event.clientY;
+                    console.log(x,' by ', y);
+            
+                    $('.click').css({'position':'absolute','left':(x+'px'),'top':(y+'px'), 'display':'block'});
         
-                $('.click').css({'position':'absolute','left':(x+'px'),'top':(y+'px'), 'display':'block'});
+                    await sleep(1000);
+        
+                    $("#closedDoor").css('display','none');
+        
+                    await sleep(1000);
+        
+                    $("#downDoor").css('display','none');
+        
+                    
+        
+        
+                    await sleep(1000)
+                    window.location.href="tent.html";
     
-                await sleep(1000);
-    
-                $("#closedDoor").css('display','none');
-    
-                await sleep(1000);
-    
-                $("#downDoor").css('display','none');
-    
-                
-    
-    
-                await sleep(1000)
-                window.location.href="tent.html";
+            }
+        });
+        
 
-        }
     });
-});
